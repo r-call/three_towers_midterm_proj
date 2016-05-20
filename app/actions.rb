d60b19b63431a3a1d2466fae8a340cce2858d483
jwleddy <jwleddy@gmail.com>
2016-05-20 06:24:30 +0000

    tweaked the view and route logic

--- app/actions.rb
+++ app/actions.rb
@@ -2,19 +2,25 @@
 #'/game' is the entry point. A new player is created when you visit. Your player ID is stored as a session cookie.
 
 get '/game' do
-  @player = Player.create
-  session[:player_id] ||= @player.id
+  if session[:player_id].nil?
+    @player = Player.create
+    session[:player_id] = @player.id
+  else
+    @player = Player.find(session[:player_id])
+  end
   # if there's a game with an open P2 spot, take that spot
   # otherwise create new game as P1
-  first_available_game = Game.where(player_2_id: nil).first
+  first_available_game = Game.where(player_1_id: !@player.id, player_2_id: nil).first
   if first_available_game.nil?
     @game = Game.create(player_1_id: @player.id)
+    @player.generate_hand(@game.id)
   else
     @game = first_available_game
     @game.update(player_2_id: @player.id)
+    @player.generate_hand(@game.id)
   end
 
-  @player.generate_hand(@game.id)
+
 
   redirect "game/1"
   # redirect "game/#{@game.id}"
@@ -25,7 +31,7 @@ end
 get '/game/:id' do
   @player = Player.find(session[:player_id])
   @cards = @player.show_cards(params[:id])
-  # @opponent = @player.find_opp(@game.id)
+  # @opponent = @player.find_opp(params[:id])
 
   erb :'game/index'
 end
