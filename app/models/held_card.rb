3ad4f2531fc74df33796f61faa5d5e62ef1dd32d
Ryan Callaway <callaway.r@gmail.com>
2016-05-19 20:03:18 -0700

    create generate hand methods in player.rb

--- app/models/held_card.rb
+++ app/models/held_card.rb
@@ -2,4 +2,8 @@ class HeldCard < ActiveRecord::Base
   belongs_to :player
   belongs_to :card
 
+  def initialize (card)
+    super()
+    card_id = card
+  end
 end 
--- app/models/player.rb
+++ app/models/player.rb
@@ -3,60 +3,57 @@ class Player < ActiveRecord::Base
   has_many :held_cards, through: :cards
   has_many :games
 
+  @turn = Game.player_1
+
+  def generate_hand
+    5.times { generate_card }
+  end
+
+  def generate_card
+    HeldCard.new(random_card_num)
+  end
+  
+  # unfinished
+  def show_cards
+    hand = []
+    #name, desc, url, type, mana_cost
+    # gold_cost, stamina_cost (positive values)
+    HeldCard.where(player_id: params[:id]).each do |card|  
+      hand << card_hash(card)
+    end
+  end
+
+  def random_card_num
+    rand(1..Card.count)
+  end
+
   def find_opp(game)   
     if game.player_1 == self    
       game.player_2   
     else    
       game.player_1   
     end   
-  end   
-  
-  #Use ActiveRecord .to_hash to get get card.     
-  #=> returns an array of hashes with result of query   
-  #then pass array of single hash into play_card    
-  def play_card (card, game)    
-    #TODO: check if it is valid to play this card   
-    opp = find_opp(game)    
-    
-    card_hash = card_to_hash(card)    
-    
-    card_hash.each_pair do |key,value|    
-      binding.pry   
-      case key    
-      when "own_mana"   
-        mana += value   
-      when "own_gold"   
-        gold += value   
-      when "own_stamina"    
-        stamina += value    
-      when "own_shield"   
-        shield += value   
-      when "own_castle"   
-        own_castle += value   
-      when "opp_mana"   
-        opp.opp_mana += value   
-      when "opp_gold"   
-        opp.opp_gold += value   
-      when "opp_stamina"    
-        opp.opp_stamina += value    
-      when "opp_shield"   
-        opp.opp_shield += value   
-      when "opp_castle"   
-        opp.opp_castle += value   
-      when "mana_cost"    
-        mana += value   
-      when "gold_cost"    
-        gold += value   
-      when "stamina_cost"   
-        stamina += value    
-      end   
-    end   
-  end   
+  end
 
-  def card_to_hash (card)   
-    c_hash = {}   
-    card.attributes.each {|k,v| c_hash[k] = v}    
-  end   
+  def card_hash(card)
+    name = card.name
+    desc = card.description
+    url = card.image_url
+    type = card.card_type
+    mana_cost *= card.mana_cost * -1
+    gold_cost *= card.gold_cost * -1
+    stamina_cost *= card.stamina_cost * -1
+
+    hash = {}
+    hash << {:name => name, 
+      :description => desc, 
+      :image_url => url,
+      :card_type => type, 
+      :mana_cost => mana_cost, 
+      :gold_cost => gold_cost,
+      :stamina_cost => stamina_cost}
+  end
+
 
   end  
 end 
