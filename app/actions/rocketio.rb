0cc0671b7a97e8f9430ed465bd66a99b6d4eb20a
jwleddy <jwleddy@gmail.com>
2016-05-22 20:17:54 +0000

    websockets work

--- app/actions/rocketio.rb
+++ app/actions/rocketio.rb
@@ -8,14 +8,48 @@
 
 io = Sinatra::RocketIO
 
-io.on :connect do |client|
-  msg = "New player #{client.session} in game: #{client.channel}"
-  io.push :announce, msg, :channel => client.channel  # to all clients in Channel "ch1"
+io.on :play do |msg, client|
+  puts "@@@@@@@@@@@@@received 'play' from browser"
+  @game = Game.find(client.channel.to_i)
+  @game.game_action(msg['action'],msg['player'].to_i,msg['card'].to_i)
+  # #individual update
+  # ind_msg =
+  # io.push :reload_hand, ind_msg, :to => client.session
+
+  #group update
+  io.push :game_updated, "update your game, something changed", :channel => client.channel
+  puts "@@@@@@@@@@@sending 'game updated' to browsers"
 end
 
-io.on :hi do |msg, client|
-  puts "Player #{client.session} says #{msg} (channel:#{client.channel})"
-  # => "client says haaaaaaaai!! (channel:ch1)"
+io.on :reload_request do |msg, client|
+  puts "@@@@@@@@@@sending response to 'reload request'"
+  #sends all game data back to client
+  # get '/games/:id/reload' do
+  @player = Player.find(msg['player'].to_i)
+  pp @player
+  @cards = @player.show_cards(msg['game'].to_i)
+  puts "@@@@@@@player id is #{msg['player']}"
+  pp @cards
+  @opponent = @player.find_opp(msg['game'])
+  @game = Game.find(msg['game'])
+  @total_data = {
+    cards: @cards,
+    player_id: @player.id,
+    player_castle: @player.castle,
+    player_shield: @player.shield,
+    player_mana: @player.mana,
+    player_stamina: @player.stamina,
+    player_gold: @player.gold,
+    opponent_id: @opponent ? @opponent.id : '',
+    opponent_castle: @opponent ? @opponent.castle : '',
+    opponent_shield: @opponent ? @opponent.shield : '',
+    opponent_mana: @opponent ? @opponent.mana : '',
+    opponent_stamina:  @opponent ? @opponent.stamina : '',
+    opponent_gold:  @opponent ? @opponent.gold : '',
+    current_player_id: @game.current_player_id,
+    current_game_winner_id: @game.winner_id,
+    current_game_loser_id: @game.loser_id
+  }
+  io.push :reload, @total_data.to_json, :to => client.session
+  puts "@@@@@@@@@ json data pushed to browser"
 end
-
-#on game join, connect to proper channel
--- public/javascript/game_sockets.js
+++ public/javascript/game_sockets.js
@@ -6,18 +6,23 @@
 // io.on("light", function(data){
 //   console.log("server light sensor : " + data.value);
 // }); // => "server light sensor : 150"
+//
+// $(document).ready(function() {
+//   var game_id = $("#data").attr('game-id');
 
-$(document).ready(function() {
-  var game_id = $("#data").attr('game-id');
+  // var io = new RocketIO({channel: game_id}).connect();  // set channel "ch1"
 
-  var io = new RocketIO({channel: game_id}).connect();  // set channel "ch1"
 
-  io.on("connect", function(){
-    io.push("hi", "Player joined game:#{game_id}");
-  });
 
-  io.on("announce", function(msg){
-    alert(msg);
-    // => alert "new client a1b2cde345fg in ch1"
-  });
-});
+  // $('.held-card').click(function() {
+  //   var card_num = $(this).attr('value');
+  //   io.push("chat", card_num);
+  // });
+
+
+
+  // io.on("announce", function(msg){
+  //   alert(msg);
+  //   // => alert "new client a1b2cde345fg in ch1"
+//   // });
+// });
